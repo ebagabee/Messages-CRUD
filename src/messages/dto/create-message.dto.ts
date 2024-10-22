3f86afc87128246da85594ee2286d404a910b019
ebagabee <2001.gabrielhas@gmail.com>
2024-10-22 19:48:42 -0300

    feat: started validation with class-validator

--- src/messages/dto/create-message.dto.ts
+++ src/messages/dto/create-message.dto.ts
@@ -1,5 +1,17 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateMessageDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
+  @MinLength(5)
   readonly text: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly from: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly to: string;
 }
